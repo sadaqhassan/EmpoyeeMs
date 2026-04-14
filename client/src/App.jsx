94099480cf74567897f85354c97d2153de4a77a3
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-04-14 13:09:00 +0300

    setup router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Routes } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+
+      <Routes>
+        
+      </Routes>
+    </div>
   )
 }
 
