d2968ab9b38b0b95e358f1a324a8bd09ffa55c6e
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-04-15 14:51:27 +0300

    layoutfixed

--- client/src/Pages/Layout.jsx
+++ client/src/Pages/Layout.jsx
@@ -4,10 +4,10 @@ import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
   return (
-    <div className=' h-screen w-full text-white'>
+    <div className=' h-screen w-full'>
         <Sidebar/>
-        <main>
-            <div>
+        <main className='flex-1 overflow-auto'>
+            <div className='max-w-400 pt-16 sm:p-6 mx-auto'>
                 <Outlet/>
             </div>
         </main>
