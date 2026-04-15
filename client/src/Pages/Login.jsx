c83d8a0552cb561200cceca4a1ce3221138e875d
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-04-15 18:42:22 +0300

    handle change

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = ({role,title}) => {
+    const [inputData,setInputData] = useState({});
+
+    const handleChange = async (e) => {
+        setInputData((prev)=>({
+            ...prev,
+            [e.target.name]:e.target.value
+        }))
+    }
   return (
     <div>
         <div className='flex flex-col md:flex-row'>
@@ -23,8 +31,8 @@ const Login = ({role,title}) => {
                 <h1 className='text-2xl md:text-3xl font-medium text-start'>{role === "admin" ? "Welcome Admin" : "Welcome Employee" } </h1>
                 <p className='mt-4 text-start text-md text-gray-700 mb-3'>{title}</p>
                 <input type="email" name='email' placeholder='Email'   className='my-5'/>
-                <input type="password"  name='password' placeholder='Password'/>
-                <input type="button" value="Login" />
+                <input onChange={handleChange} type="password"  name='password' placeholder='Password'/>
+                <input onChange={handleChange} type="button"  value="Login" />
                 <p className='mt-4 text-start text-xs text-gray-700'> &#169; { new Date().getFullYear() } Developed by sadak_Dev</p>
             </div>
         </div>
